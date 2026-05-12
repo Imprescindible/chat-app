388bf3ebe1b95a64de4dcda83273cae1a93d78dd
Artem <swg.ignore@email.com>
2026-05-12 21:00:37 +0300

    fix hardcoded localhost url

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,4 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: { port: 5173 },
+  define: {
+    "import.meta.env.VITE_API_URL": JSON.stringify("https://chat-app-mxjp.onrender.com"),
+  },
 });
